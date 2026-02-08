aa6376d8a8099e998d80f9a4cb4192176db624f7
MaximumReality <lorith79@gmail.com>
2026-02-08 11:35:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-const CACHE_NAME = 'azulos-v1';
+const CACHE_NAME = 'azulos-v1.3'; // Bumped version to force iPhone to refresh
 const ASSETS = [
   'index.html',
   'manifest.json',
+  'favicon.png',          // Added for the Home Screen icon
+  'corporate-azul.png',   // Added for the Splash Screen
+  'mochkil-skate.png',    // Added for the Splash Screen
+  'mochkil-living.png',   // Added for the Splash Screen
   'hard-cyberpunk.mp3',
   'litter-box.png',
   'smokin.png',
   'mochkil-burger.png',
-  // Add all your cereal .png filenames here!
   'winter_azulos.png',
   'azulos_galaxy_mix.png',
   'azulos_neon_crunch.png',
